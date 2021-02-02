6fdb9fe962800a2d35ba0c2772575ea6c5338657
gggeek <giunta.gaetano@gmail.com>
2021-02-02 09:05:28 +0000

    revert removal of ezmatrix field type support

---
+++ Core/FieldHandler/EzMatrix.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Kaliop\eZMigrationBundle\Core\FieldHandler;
+
+use Kaliop\eZMigrationBundle\API\FieldDefinitionConverterInterface;
+use Kaliop\eZMigrationBundle\API\FieldValueConverterInterface;
+
+/**
+ * @todo make fieldSettings independent from version of ezmatrix in use, by converting them from a common format
+ */
+class EzMatrix extends AbstractFieldHandler implements FieldValueConverterInterface
+{
+    public function hashToFieldValue($fieldValue, array $context = array())
+    {
+        /// @todo resolve refs ?
+
+        $rows = array();
+        foreach($fieldValue as $data) {
+            $rows[] = new \EzSystems\EzPlatformMatrixFieldtype\FieldType\Value\Row($data);
+        }
+        return new \EzSystems\EzPlatformMatrixFieldtype\FieldType\Value($rows);
+}
+
+    public function fieldValueToHash($fieldValue, array $context = array())
+    {
+        $data = array();
+
+        foreach($fieldValue->getRows() as $row) {
+            $data[] = $row->getCells();
+        }
+
+        return $data;
+    }
+}
